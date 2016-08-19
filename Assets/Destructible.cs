bcccd9ce9c41d7261f5cb23cc03bebad297bafbe
Raicuparta <raicuparta@gmail.com>
2016-08-20 00:32:10 +0100

    destructibles

---
+++ Assets/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (MeshCollider))]
+public class Destructible : MonoBehaviour {
+    void Start() {
+        foreach (Transform child in transform) {
+            child.gameObject.AddComponent<Fracture>();
+            child.gameObject.AddComponent<Rigidbody>();
+            child.GetComponent<MeshCollider>().convex = true;
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.layer != 9) return;
+
+        GetComponent<MeshCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Fracture.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fracture : MonoBehaviour {
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.layer != 10) return;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
