429f86180ed53e1c9f9285dea0ac6aa6a309d381
Raicuparta <raicuparta@gmail.com>
2016-08-19 09:55:10 +0100

    smooth camera

--- Assets/Scripts/CharacterInput.cs
+++ Assets/Scripts/CharacterInput.cs
@@ -22,6 +22,8 @@ public class CharacterInput : MonoBehaviour {
 
         Vector3 mouse = CrossPlatformInputManager.mousePosition;
         CharacterHands.Grab(right, left, mouse);
-        
+
+        bool jump = CrossPlatformInputManager.GetButton("Jump");
+        if (jump) Movement.ChargeJump();
     }
 }
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,14 +5,17 @@ public class CharacterMovement : MonoBehaviour {
     Rigidbody Body;
     Steps CharacterSteps;
     public float MovementSpeed = 10; // character movement speed multiplier
-    public float RotationSpeed = 10;
+    public float RotationSpeed = 10; // character rotation speed multiplier
+    public float JumpChargeHeight = 3; // how much the body is lowered when chargind a jump
+    public float JumpChargeSpeed = 10; // how fast the body is lowered when chargind a jump
 
-    void Start () {
+    void Start() {
         Body = GetComponent<Rigidbody>();
+        Body.centerOfMass = -transform.up * 5;
         CharacterSteps = GetComponent<Steps>();
     }
 	
-	public void Move (float h, float v) {
+	public void Move(float h, float v) {
         // use v for forward movement
         Vector3 forwardForce = transform.rotation * Vector3.forward * v * MovementSpeed;
         forwardForce = Vector3.ProjectOnPlane(forwardForce, Vector3.up);
@@ -22,5 +25,11 @@ public class CharacterMovement : MonoBehaviour {
         Vector3 torque = Vector3.up * h * RotationSpeed;
         Body.AddTorque(torque);
         CharacterSteps.Move();
+    }
+
+    public void ChargeJump() {
+        Vector3 target = transform.position - transform.up * JumpChargeHeight;
+        Vector3 force = Util.ForceTowards(transform.position, target, JumpChargeSpeed);
+        Body.AddForce(force);
     }
 }
