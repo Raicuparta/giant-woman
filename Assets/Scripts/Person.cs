e85ecf6d302d51d9141d1d2c24e3fafd1b2106bf
Raicuparta <raicuparta@gmail.com>
2016-08-31 10:53:46 +0100

    better physics for little people

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -11,6 +11,11 @@ public class Person : Grabbable {
 
 	void Start() {
         Body = GetComponent<Rigidbody>();
+
+        // Lower the center of gravity to keep the little guy standing up
+        Body.centerOfMass = Vector3.down;
+
+        // Change the direction of movement every so often
         InvokeRepeating("ChangeDirection", 0, RotationInterval);
     }
 
@@ -20,7 +25,16 @@ public class Person : Grabbable {
     }
 	
 	void FixedUpdate () {
-        Body.velocity = transform.forward * Speed;
+        Vector3 forward = Vector3.ProjectOnPlane(transform.forward, Vector3.up);
+        Body.AddForce(forward, ForceMode.VelocityChange);
+
+        // Change the X and Z components of the velocity to the given speed,
+        // but keep the Y component as to not affect gravity.
+        Vector2 velocityXZ = new Vector2(Body.velocity.x, Body.velocity.z);
+        velocityXZ = velocityXZ.normalized * Speed;
+        Body.velocity = new Vector3(velocityXZ.x, Body.velocity.y, velocityXZ.y);
+
+        // Update the body rotation
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation, TargetRotation, RotationSpeed);
         Body.MoveRotation(rotation);
 	}
